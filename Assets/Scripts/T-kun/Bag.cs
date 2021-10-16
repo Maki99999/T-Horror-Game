e20b00ed1d12bfc60dea6a62ece872a458bb3933
Maik <maki99999@web.de>
2021-10-16 19:25:00 +0200

    various visual improvements

--- Assets/Scripts/T-kun/Bag.cs
+++ Assets/Scripts/T-kun/Bag.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity​Engine.Experimental.Rendering.Universal;
 
 namespace T
 {
@@ -9,9 +10,10 @@ namespace T
         public Collider2D bagCollider;
         public Transform playerBagTransform;
         public Collider2D playerCollider;
-        public Game.Core.Rendering.LineRenderer2D lineRenderer;
+        public Transform lineTransform;
 
-        private Vector3 lastDistance = Vector3.zero;
+        private float lastDistance = 0;
+        private Vector3 lastDistanceVector = Vector3.zero;
 
         private void Start()
         {
@@ -25,18 +27,23 @@ namespace T
 
         private void Update()
         {
-            lastDistance = transform.position + transform.up * 0.5f - playerBagTransform.position;
-            lineRenderer.PointB = lastDistance;
+            Debug.DrawRay(playerBagTransform.position, Vector3.up);
+            lastDistanceVector = transform.position + transform.up * 0.5f - playerBagTransform.position;
+            lastDistance = lastDistanceVector.magnitude;
+            lineTransform.localScale = new Vector3(2, 2 + lastDistance * 16, 2);
+
+            float angle = Mathf.Atan2(lastDistanceVector.y, lastDistanceVector.x) * Mathf.Rad2Deg;
+            lineTransform.rotation = Quaternion.AngleAxis(angle + 90f, Vector3.forward);
         }
 
         private IEnumerator CheckDistance()
         {
             while (isActiveAndEnabled)
             {
-                if (lastDistance.sqrMagnitude > 16f)
+                if (lastDistance > 4f)
                 {
                     bagCollider.enabled = false;
-                    yield return new WaitForSeconds(2f);;
+                    yield return new WaitForSeconds(2f);
                     bagCollider.enabled = true;
                 }
                 yield return new WaitForSeconds(1f);
